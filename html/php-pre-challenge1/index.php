af4ab8a94e6ce28acbe715556c482578a069db51
MUnehiroMachida <Life1231@outlook.jp>
2020-05-11 14:32:31 +0900

    練習課題1_１回目レビュー後

--- html/php-pre-challenge1/index.php
+++ html/php-pre-challenge1/index.php
@@ -1,17 +1,12 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
-    if($i % 3 == 0 && $i % 5 == 0){
-        echo ('3の倍数であり、5の倍数');
-        echo '<br>'; 
-    }elseif($i % 3 == 0){
-        echo('3の倍数');
-        echo '<br>'; 
-    }elseif($i % 5 == 0){
-        echo('5の倍数');
-        echo '<br>'; 
+    if($i % 15 === 0){
+        echo ('3の倍数であり、5の倍数'.'<br>');
+    }elseif($i % 3 === 0){
+        echo('3の倍数'.'<br>');
+    }elseif($i % 5 === 0){
+        echo('5の倍数'.'<br>');
     }else{
-        echo $i;
-        echo '<br>'; 
+        echo "$i<br>";
     }
 }
-?>
