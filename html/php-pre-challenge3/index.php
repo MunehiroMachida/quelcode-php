cc164f0ab480359d53022f892e5a01e3a40d7c2d
MUnehiroMachida <Life1231@outlook.jp>
2020-05-15 15:33:09 +0900

    5/15直し

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -4,26 +4,15 @@ $limit = $_GET['target'];
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
-
 try{
     $db = new PDO($dsn,$dbuser,$dbpassword);
-    echo "<p>DB接続に成功しました。</p>";
-    // SQL実行	
-    $sql = "SELECT value FROM prechallenge3";
-    $stmt = $db->prepare($sql);	//prepareでSQL文を実行
-    $stmt->execute();	//executeは実行するっていう意味
-    // 結果の取得	
-    $record = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 }catch (PDOException $e){
     echo 'DB接続エラー:'.$e->getMessage();
-    
 }
+$records = $db->query('SELECT value FROM prechallenge3');
+$record = $records->fetchAll(PDO::FETCH_ASSOC);
+    print($record[0]."\n");
 
 echo "<pre>";
 print_r($record);
 echo "</pre>";
-
-
-
-
