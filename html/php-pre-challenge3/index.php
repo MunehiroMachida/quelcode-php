e840a28cf6f7f795940ee2e83c1d873ac8f345dd
MUnehiroMachida <Life1231@outlook.jp>
2020-05-21 13:32:22 +0900

    5/21文字列での出力

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -35,53 +35,20 @@ function kumiawase($zentai,$nukitorisu){
         return $arrs;
     }
 
-// 要件
-// ・functionの中の動きは気にしなくていいので下の$tempsの中身１５パターン取り出したい。
-// 　  やること
-// 　　 ┗ $tempsの要素の個数を出す
-// 　　　$i < $tempsの数繰り返すfor文の作成
-// 　　　配列の中から、１つ取り出す処理
-// 　　　配列の中から、2つ取り出す処理
-// 　　　配列の中から、3つ取り出す処理
-// 　　　配列の中から、4つ取り出す処理
-//      を出力
 
-
-$i = 1;
-$temps=kumiawase(array(1,2,3,4),$i); //$iは取り出す桁数。下のforで、1桁→2桁→3桁→4桁の全パターンを取り出す。
-$array_amount = count($temps); //配列の中身の数を数える　　数えるために↑が必要
-
-
-// while($i <= $array_amount){ //$iは1。$array_amountは4。$iが$array_amount以下の場合、この場合は4まで繰り返す。
-//     $temps=kumiawase(array(1,2,3,4),$i);  //←ここなぜ再度入れる必要があるか考え中
-//     print "<ul>";
-//     foreach($temps as $temp){
-//         print "<li>".implode($temp)."</li>";
-//     }
-//     print "</ul>";
-//     $i++;
-// }
+$items = [1,2,3,4];
+$array_amount = count($items);
+$str_box = '';
 
 
 for($i = 1; $i <= $array_amount; $i++){
-    $temps=kumiawase(array(1,2,3,4),$i);  //←ここなぜ再度入れる必要があるか考え中
-    print "<ul>";
+    $temps=kumiawase($items,$i);
     foreach($temps as $temp){
-        print "<li>".implode($temp)."</li>";
+        $str_box .= implode($temp).','.'<br>';
     }
-    print "</ul>";
+    
 }
 
-
-
-
-
-
-
-// print "<ul>";
-// foreach($temps as $temp){
-//     print "<li>".implode($temp)."</li>";
-// }
-// print "</ul>";
-
-// var_dump($i.'aa'.'<br>');
+echo'<pre>';
+print ($str_box);
+echo'</pre>';
