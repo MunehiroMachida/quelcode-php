28f88bf12b39d1e3dd7a5ec494958ed0b81a5c94
MUnehiroMachida <Life1231@outlook.jp>
2020-05-27 13:11:49 +0900

    5/27インデント、nullなど修正

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -1,14 +1,15 @@
 <?php
 $limit = $_GET['target'];
 
-if(strpos($limit,'.0')){
-    http_response_code(400);
-    exit();
-}
 
-if($limit <= 0 or $limit != (integer)$limit or $limit != is_numeric($limit)){
+if($limit <= 0
+    or $limit != (int)$limit
+    or $limit != is_numeric($limit)
+    or strpos($limit,'.0')){
+
     http_response_code(400);
     exit();
+
 }else{
     $dsn = 'mysql:dbname=test;host=mysql';
     $dbuser = 'test';
@@ -25,9 +26,9 @@ if($limit <= 0 or $limit != (integer)$limit or $limit != is_numeric($limit)){
 
     //一旦valuカラムから全部の数字を取り出して単次元配列にする処理
     $array_amount = count($record);
-        for($i = 0; $i < $array_amount; $i++){
-            $record_array[] = (int)$record[$i]['value'];
-        }
+    for($i = 0; $i < $array_amount; $i++){
+        $record_array[] = (int)$record[$i]['value'];
+    }
     //組み合わせを出す関数
     //参考https://stabucky.com/wp/archives/2188
     function set_array($total,$digit){  //$totalが$record_array。$record_arrayは上で単次元に直したやつ。$digitは桁数。
@@ -53,30 +54,24 @@ if($limit <= 0 or $limit != (integer)$limit or $limit != is_numeric($limit)){
     //全パターンの組み合わせを$array_boxに入れる処理
     $array_box = [];
     for($i = 1; $i <= $array_amount; $i++){
-        if(empty($array_box)){
-            $temps = set_array($record_array,$i);
-            $array_box = $temps;
-        }else{
-            $temps = set_array($record_array,$i);
-            $array_box = array_merge($array_box,$temps);
-        }
+        $temps = set_array($record_array,$i);
+        $array_box = array_merge($array_box,$temps);
     }
 
     //$limitの組み合わせを新しい配列に入れる処理
     $limit = (int)$limit;
     for($i = 0; $i < count($array_box); $i++){
-        for($j = 0; $j < count($array_box[$i]); $j++){
             if($limit === array_sum($array_box[$i])){
                 $sum_limit[] = $array_box[$i];
-                break;
-            }
         }
     }
 
     //$limitをjsonに変換して出力
-    $sum_limit_json = json_encode($sum_limit);
+    if(is_null($sum_limit)){
+        $sum_limit_json = '[]';
+    }else{
+        $sum_limit_json = json_encode($sum_limit);
+    }
 
-    echo'<pre>';
-    print_r($sum_limit_json);
-    echo'</pre>';
+    echo($sum_limit_json);
 }
