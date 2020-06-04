da61ed4294b0733f8a4b1e8af9a02d78ffeefc7a
MUnehiroMachida <Life1231@outlook.jp>
2020-06-04 12:44:02 +0900

    6/3いいねを押すとinsertとdeleteを繰り返す処理

--- html/php-challenge/like.php
+++ html/php-challenge/like.php
@@ -8,34 +8,54 @@ if (isset($_SESSION['id'])) {//ここはmemberのid。
     $id = $_REQUEST['id'];//これはpostsのid
 	
 	// 投稿を検査する
-	$goods = $db->prepare('SELECT * FROM goods WHERE id=?');
+	$goods = $db->prepare('SELECT * FROM goods');
 	$goods->execute(array($id));
-	$good = $goods->fetch();
+    $good = $goods->fetchAll(PDO::FETCH_ASSOC);
 
-        //goodsテーブルに値が入っていて、member_idと操作してる人が一緒で、投稿のidとリクエストで飛んできたidが一緒の時
-        // if(!empty($good) && $good['member_id'] == $_SESSION['id'] && $good['post_id'] == $id){
-        //     //消す。つまりいいねではない状態にする
-        //     $del = $db->prepare('DELETE FROM goods WHERE id=?');
-        //     $del->execute(array($id));
-        // }
-        // else{
-        //     ///そうではないときいいね状態にする。
-        //     $insert = $db->prepare('INSERT INTO goods SET $_GET');
-        //     $insert->execute(array($id));
-        // }
-        
-
-// とりあえず、いいねテーブルに打ち込みたい
-
-//6/3やること。挙動が少しおかしい。データベースに投稿のidとmenberidを入れることは成功
+    //レコード数を数えてくれる
+    $count_sql = 'SELECT COUNT(*) FROM goods';
+    $stmt = $db->query($count_sql);
+    $count = (int)$stmt->fetchColumn();
 
 
-    if(empty($good)){
+    $is_good['session'] = $_SESSION['id'];
+    $is_good['get'] = $_GET['id'];
+
+    $a = '';
+    if(empty($good)){ //何も値が入っていない時。
+        $insert = $db->prepare('INSERT INTO goods SET post_id=?, member_id=?');
+        $insert->bindParam(1,$_GET['id']);
+        $insert->bindParam(2,$_SESSION['id']);
+        $insert->execute();
+    }elseif(!empty($good)){//値が入っている
+        for($i=0;$i<$count;$i++){ //同じ値があるかけんさ
+            if($is_good['get'] == $good[$i]['post_id'] && $is_good['session'] == $good[$i]['member_id']){
+                $a = 'true';
+                $goods_id = $good[$i]['id'];
+            }
+        }
+        if($a == 'true'){
+            $del = $db->prepare('DELETE FROM goods WHERE id=?');
+            $del->execute(array($goods_id));
+        }elseif($a != 'true'){
             $insert = $db->prepare('INSERT INTO goods SET post_id=?, member_id=?');
             $insert->bindParam(1,$_GET['id']);
             $insert->bindParam(2,$_SESSION['id']);
             $insert->execute();
+        }
     }
+
+    echo '<pre>';
+    print_r($good); 
+    echo '</pre>';
+
+    echo '<pre>';
+    print_r($is_good); 
+    echo '</pre>';
+
+
+    var_dump($a);
+
 }
 
 header('Location: index.php'); exit();
